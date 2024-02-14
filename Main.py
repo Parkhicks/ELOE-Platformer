a8aea251481a8da32ace65a41ff5e5e7ed295f1f
shaunvann <shaunvann36@gmail.com>
2024-02-14 13:24:59 -0700

    Updating README and adding comments

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 # intial Code from https://docs.replit.com/tutorials/python/2d-platform-game
-import pygame, numpy
+import pygame, numpy # How to install numpy? I need to find out so I can add to the README
 
 WIDTH = 400
 HEIGHT = 300
